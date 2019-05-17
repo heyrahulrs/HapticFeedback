3bb31db7b0de5a36835acb34c3f88c6b2842eaf0
Rahul <rahulrs0029@gmail.com>
2019-05-17 16:03:52 +0530

    Added 7 types of Haptic Feedbacks.

--- HapticFeedback/ViewController.swift
+++ HapticFeedback/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let feedbackSupportLevel = UIDevice.current.value(forKey: "_feedbackSupportLevel") as? Int ?? 1
+        let isHapticAvailable = feedbackSupportLevel == 2
+        
+        if !isHapticAvailable {
+            
+            buttons.forEach { $0.isEnabled = false }
+
+            let alert = UIAlertController(title: "Haptic Feedback not available!", message: nil, preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            
+            alert.addAction(action)
+            present(alert, animated: true)
+        }
+        
+    }
+    
+    @IBAction func didTapImpactFeedbackGenerator(_ sender: UIButton){
+        let style = UIImpactFeedbackGenerator.FeedbackStyle(rawValue: sender.tag)
+        let impactFeedbackGenerator = UIImpactFeedbackGenerator(style: style ?? .medium)
+        impactFeedbackGenerator.impactOccurred()
+    }
+    
+    @IBAction func didTapSelectionFeedbackGeneratorButton(){
+        let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+        selectionFeedbackGenerator.selectionChanged()
+    }
+    
+    @IBAction func didTapNotificationFeedbackGenerator(_ sender: UIButton){
+        let style = UINotificationFeedbackGenerator.FeedbackType(rawValue: sender.tag)
+        let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+        notificationFeedbackGenerator.notificationOccurred(style ?? .success)
     }
 
 
