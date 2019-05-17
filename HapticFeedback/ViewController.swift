188e4c036a2f7f2d892212efd80c0e680cc05507
Rahul <rahulrs0029@gmail.com>
2019-05-17 15:28:08 +0530

    Initial Commit

---
+++ HapticFeedback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticFeedback
+//
+//  Created by Rahul Sharma on 5/17/19.
+//  Copyright © 2019 Rahul Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
